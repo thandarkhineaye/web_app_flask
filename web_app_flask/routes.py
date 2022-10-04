720f9532debed21fad99571b475498c0db360a84
thandarkhineaye <thandar.khine.aye@airitech.co.jp>
2022-10-04 23:45:11 +0900

    add bootstraps styles

--- web_app_flask/routes.py
+++ web_app_flask/routes.py
@@ -1,12 +1,26 @@
 from flask import render_template
 from web_app_flask import app
-@app.route('/')
-def main():
-    return 'Hello from Main Page!!!'
 
+@app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', title='Home Page')
+
+@app.route('/account')
+def account():
+    return render_template('Account.html', title='Account')
+
+@app.route('/about')
+def about():
+    return render_template('About.html', title='About Us')
+
+@app.route('/register')
+def register():
+    return render_template('Register.html', title='Sign Up')
+
+@app.route('/signin')
+def signin():
+    return render_template('SignIn.html', title='Log In')
 
 @app.errorhandler(404)
 def page_not_found(e):
