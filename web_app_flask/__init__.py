259046ed1b2ee0b6823b9271238e5a27585640d1
thandarkhineaye <thandar.khine.aye@airitech.co.jp>
2022-11-01 17:31:20 +0900

    add reset password and userdetails form

--- web_app_flask/__init__.py
+++ web_app_flask/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'thisisfirstflaskapp'
@@ -14,6 +16,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = ''
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+
+# reference to Gmil SMPT setup setting 
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'tkatesting@gmail.com'
+app.config['MAIL_PASSWORD'] = ''
+
+# instance for mail
+mail = Mail(app)
 
 
 from web_app_flask import routes
